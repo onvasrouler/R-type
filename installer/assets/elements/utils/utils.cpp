8e1411e6f0055ba5071fb09df4f2548379e6e1d8
aimericdrk <aimeric.rouyer@epitech.eu>
2024-09-23 18:58:32 +0200

    :heavy_plus_sign: adding the utils elements

---
+++ installer/assets/elements/utils/utils.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2024
+** R-type
+** File description:
+** utils
+*/
+
+#include "utils.hpp"
---
+++ installer/assets/elements/utils/utils.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** R-type
+** File description:
+** utils
+*/
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <thread>
+#include <chrono>
+#include <algorithm>
+#include <fstream>
