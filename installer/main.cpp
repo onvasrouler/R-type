0cabc8d5ac21bbc2a4a374ba9a92f295fab8aefc
aimericdrk <aimeric.rouyer@epitech.eu>
2024-09-23 18:57:28 +0200

    :heavy_plus_sign: adding a main.cpp to be able to compile it ( will be combined with the client later )

---
+++ installer/main.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** R-type
+** File description:
+** main
+*/
+
+#include "install.hpp"
+
+int main(int ac, char **av)
+{
+    Installer installer;
+    installer.run();
+    return 0;
+}
