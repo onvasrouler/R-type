2b9e14b11f767a678b734fded193206c43b9cc28
aimericdrk <aimeric.rouyer@epitech.eu>
2024-09-24 18:00:59 +0200

    :coffin: remove usless code

--- client/main.cpp
+++ client/main.cpp
@@ -16,18 +16,16 @@
 
 int main() {
     // Create a Raylib window
-    RlibWindow window("window_settings.json");
+    RlibWindow window("./config/window_settings.json");
     window.InitRlib();
     
     // Create a FpsCounter
     std::unique_ptr<FpsCounter> rlibFpsCounter = std::make_unique<FpsCounter>(100, 0, 30, BLACK);
-    RaylibText text("Hello, Raylib!", 200, 0, 30, BLACK);
     window.setFpsCounter(std::move(rlibFpsCounter));
     // Main game loop
     while (!window.ShouldClose()) {
         window.ClearRlibBackground();
         window.BeginRlibDraw();
-        text.DrawRlibText();
         window.EndRlibDraw();
     }
     return 0;
