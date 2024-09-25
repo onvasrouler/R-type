a960e148b21bc35c9df020c19fd127fc754a365e
aimericdrk <aimeric.rouyer@epitech.eu>
2024-09-25 11:15:29 +0200

    :building_construction: moving include in a file to avoid repetition

---
+++ client/src/include.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** R-type
+** File description:
+** include
+*/
+
+#pragma once
+
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>  
+#include <raylib.h>
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <fstream>
