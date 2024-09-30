b6a0336fefde111e1e358c769a8e2ede5fe53862
julien <julien.renard@epitech.eu>
2024-09-30 17:09:15 +0200

    :sparkles: add first game method with french commentary

---
+++ server/src/game/Game.cpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Game
+*/
+
+#include "Game.hpp"
+
+Game::Game()
+{
+    this->init_game();
+}
+
+
+Game::~Game()
+{
+
+}
+
+static void player_loop()
+{
+
+}
+
+static void game_loop()
+{
+    //boucle infini qui si il n'y a pas de player attend un player
+}
+
+void Game::init_game()
+{
+    //créer la connxion
+    //créer le thread principal -> game_loop
+}
