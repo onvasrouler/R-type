4e97f3d2a0c71a96231b6d41991ab5952882c0c6
julien <julien.renard@epitech.eu>
2024-09-30 13:41:09 +0200

    :sparkles: add Player.cpp with Player class inherit from AEntity.cpp

---
+++ server/src/entity/Player.cpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Player
+*/
+
+#include "Player.hpp"
+
+Player::Player(int x, int y, int l, int h) : AEntity(x, y, l, h)
+{
+
+}
+
+Player::~Player()
+{
+
+}
+
+void Player::move_entity()
+{
+
+}
