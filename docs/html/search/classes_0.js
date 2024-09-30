eb6c3b62281b3db2ea522f1395f5159c11ab49d9
Antoinegtir <antoine.gonthier@epitech.eu>
2024-09-30 20:40:00 +0200

    🔥 remove useless generated docs files

--- docs/html/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['udperror_0',['UDPError',['../class_u_d_p_error.html',1,'']]],
-  ['udpserver_1',['UDPServer',['../class_u_d_p_server.html',1,'']]]
-];
--- docs/html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['what_0',['what',['../class_u_d_p_error.html#abb6e104720a2584980b8a767b8489d46',1,'UDPError']]]
-];
