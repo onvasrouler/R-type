3e94ab716dd808375d41154770d04751f279d8f6
Antoine Gonthier <114834504+Antoinegtir@users.noreply.github.com>
2024-09-30 17:08:57 +0200

    Revert "Server axios"

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,22 +1,5 @@
-/*
-** EPITECH PROJECT, 2024
-** Server UDP R-Type
-** File description:
-** main.cpp
-*/
-
-#include "UDPError.hpp"
-#include "UDPServer.hpp"
+#include <iostream>
 
 int main() {
-    try {
-        boost::asio::io_context io_context;
-        UDPServer server(io_context, 3000);
-        io_context.run();
-    } catch (const UDPError& e) {
-        std::cerr << "Caught UDPError: " << e.what() << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "Caught exception: " << e.what() << std::endl;
-    }
     return 0;
 }
