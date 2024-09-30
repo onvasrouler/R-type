59cc3a9c9cc3b0cf8dfff919183a096591269442
julien <julien.renard@epitech.eu>
2024-09-30 13:39:42 +0200

    :sparkles: add IEntity.hpp file with interface of entities

---
+++ server/src/entity/IEntity.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** IEntity
+*/
+
+#pragma once
+
+#include <memory>
+ 
+ class IEntity
+ {
+ public:
+    virtual ~IEntity() = default;
+
+    virtual void move_entity() = 0;
+
+    virtual void set_l(int l) = 0;
+    virtual void set_h(int h) = 0;
+    virtual void set_x(int x) = 0;
+    virtual void set_y(int y) = 0;
+
+    virtual int get_x(void) = 0;
+    virtual int get_y(void) = 0;
+    virtual int get_l(void) = 0;
+    virtual int get_h(void) = 0;
+ };
