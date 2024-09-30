dc937f2da04dbea14b915339d85a0a3beadb8b0c
julien <julien.renard@epitech.eu>
2024-09-30 13:41:49 +0200

    :sparkles: add Point.cpp with Point class

---
+++ server/src/tools/Point.cpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Point
+*/
+
+#include "Point.hpp"
+
+Point::Point()
+{
+    this->_x = 0;
+    this->_y = 0;
+}
+
+Point::Point(int x, int y)
+{
+    this->_x = x;
+    this->_y = y;
+}
+
+Point::~Point()
+{
+
+}
+
+void Point::set_x(int x)
+{
+    this->_x = x;
+}
+
+void Point::set_y(int y)
+{
+    this->_y = y;
+}
+
+int Point::get_x()
+{
+    return (this->_x);
+}
+
+int Point::get_y()
+{
+    return (this->_y);
+}
