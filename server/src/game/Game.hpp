4b7b1211bf1b1a9ba3a9d365ccb112b076dbd15e
julien <julien.renard@epitech.eu>
2024-09-30 17:08:52 +0200

    :sparkles: add first game class with french commentary

---
+++ server/src/game/Game.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Game
+*/
+
+class Game
+{
+public:
+    Game();
+    ~Game();
+
+    void init_game();
+
+    //manage container
+
+private:
+    //include le container
+};
+
