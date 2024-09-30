9df4653b61a085d23eaadb3e4397e775bd389a0d
julien <julien.renard@epitech.eu>
2024-09-30 13:42:08 +0200

    :sparkles: add Point.cpp with Rect class

---
+++ server/src/tools/Rect.cpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Thpe
+** File description:
+** Rect
+*/
+
+#include "Rect.hpp"
+
+Rect::Rect()
+{
+    this->_l = 0;
+    this->_h = 0;
+}
+
+Rect::Rect(int l, int h)
+{
+    this->_l = l;
+    this->_h = h;
+}
+
+Rect::~Rect()
+{
+
+}
+
+void Rect::set_l(int l)
+{
+    this->_l = l;
+}
+
+void Rect::set_h(int h)
+{
+    this->_h = h;
+}
+
+int Rect::get_l()
+{
+    return (this->_l);
+}
+
+int Rect::get_h()
+{
+    return (this->_h);
+}
