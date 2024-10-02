2f7bb91a21630d13546a790bbd8d4f81bfc06689
aimericdrk <aimeric.rouyer@epitech.eu>
2024-10-02 16:02:09 +0200

    :bug: fix the reaction of the button to click and hoover

--- client/src/elements/raylibButton/raylibButton.cpp
+++ client/src/elements/raylibButton/raylibButton.cpp
@@ -13,11 +13,12 @@ RaylibButton::RaylibButton()
     _pos = {0, 0};
     _size = {100, 50};
     _color = WHITE;
-    _type = BUTTON;
+    _DefaultColor = WHITE;
+    _type = PBUTTON;
     _isHovered = false;
     _isClicked = false;
     _isPressed = false;
-    _isReleased = false;
+    _isReleased = true;
     _isFocused = false;
     _isDragged = false;
     _isDropped = false;
@@ -26,17 +27,18 @@ RaylibButton::RaylibButton()
     _isToggled = false;
 }
 
-RaylibButton::RaylibButton(std::string text, Vector2 pos, Vector2 size, Color color, ButtonType type)
+RaylibButton::RaylibButton(std::string text, Vector2 pos, Vector2 size, Color color, PButtonType type)
 {
     _text = text;
     _pos = pos;
     _size = size;
     _color = color;
+    _DefaultColor = color;
     _type = type;
     _isHovered = false;
     _isClicked = false;
     _isPressed = false;
-    _isReleased = false;
+    _isReleased = true;
     _isFocused = false;
     _isDragged = false;
     _isDropped = false;
@@ -49,32 +51,24 @@ void RaylibButton::DrawRlibButton() const
 {
     if (_isHidden)
         return;
+
     DrawRectangle(_pos.x, _pos.y, _size.x, _size.y, _color);
     DrawText(_text.c_str(), _pos.x + _size.x / 2 - MeasureText(_text.c_str(), 20) / 2, _pos.y + _size.y / 2 - 10, 20, BLACK);
 }
 
-void RaylibButton::UpdateRlibButton()
+void RaylibButton::UpdateRlibButton() const
 {
-    if (_isDisabled)
-        _color = GRAY;
-    else if (_isHovered)
-        _color = LIGHTGRAY;
-    else
-        _color = WHITE;
-    if (_isHidden || _isDisabled)
-        return;
     Vector2 mousePos = GetMousePosition();
     if (CheckCollisionPointRec(mousePos, { _pos.x, _pos.y, _size.x, _size.y })) {
-        _isHovered = true;
+        const_cast<RaylibButton*>(this)->setHovered(true);
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-            _isPressed = true;
+            const_cast<RaylibButton*>(this)->setPressed(true);
         if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
-            _isReleased = true;
+            const_cast<RaylibButton*>(this)->setReleased(true);
     } else {
-        _isHovered = false;
-        _isPressed = false;
-        _isReleased = false;
+        const_cast<RaylibButton*>(this)->setHovered(false);
     }
+    DrawRlibButton();
 }
 
 void RaylibButton::SetText(std::string text)
@@ -97,9 +91,33 @@ void RaylibButton::SetColor(Color color)
     _color = color;
 }
 
-void RaylibButton::setType(ButtonType type)
+void RaylibButton::setType(PButtonType type)
 {
     _type = type;
+}
+
+void RaylibButton::setHovered(bool hovered)
+{
+    _isHovered = hovered;
+    if (_isHovered && !_isPressed && !_isReleased)
+        _color = LIGHTGRAY;
+    else
+        _color = _DefaultColor;
+}
+
+void RaylibButton::setPressed(bool pressed)
+{
+    _isPressed = pressed;
+    _isReleased = false;
+    if (_isPressed)
+        _color = GRAY;
+}
+
+void RaylibButton::setReleased(bool released)
+{
+    _isReleased = released;
+    if (_isReleased)
+        _color = _DefaultColor;
 }
 
 std::string RaylibButton::GetText()
@@ -122,7 +140,7 @@ Color RaylibButton::GetColor()
     return _color;
 }
 
-ButtonType RaylibButton::GetType()
+PButtonType RaylibButton::GetType()
 {
     return _type;
 }
--- client/src/elements/raylibButton/raylibButton.hpp
+++ client/src/elements/raylibButton/raylibButton.hpp
@@ -9,40 +9,45 @@
 
 #include "../../include.hpp"
 
-enum ButtonType {
-    BUTTON,
-    TOGGLE,
-    ERROR
+enum PButtonType {
+    PBUTTON,
+    PTOGGLE,
+    PERROR
 };
 
 class RaylibButton {
     public:
     RaylibButton();
-    RaylibButton(std::string text, Vector2 pos, Vector2 size, Color color, ButtonType type);
+    RaylibButton(std::string text, Vector2 pos, Vector2 size, Color color, PButtonType type);
     ~RaylibButton() = default;
 
     void DrawRlibButton() const;
 
-    void UpdateRlibButton();
+    void UpdateRlibButton() const;
 
     void SetText(std::string text);
     void SetPos(Vector2 pos);
     void SetSize(Vector2 size);
     void SetColor(Color color);
-    void setType(ButtonType type);
+    void setType(PButtonType type);
+
+    void setHovered(bool hovered);
+    void setPressed(bool pressed);
+    void setReleased(bool released);
 
     std::string GetText();
     Vector2 GetPos();
     Vector2 GetSize();
     Color GetColor();
-    ButtonType GetType();
+    PButtonType GetType();
     
     private:
-    ButtonType _type;
+    PButtonType _type;
     std::string _text;
     Vector2 _pos;
     Vector2 _size;
     Color _color;
+    Color _DefaultColor;
     bool _isHovered;
     bool _isClicked;
     bool _isPressed;
