fe1592b9a7f2b3b5b0c1762d37d9394bd998955f
aimericdrk <aimeric.rouyer@epitech.eu>
2024-10-02 17:51:51 +0200

    :building_construction: add every adder and setter of the elements for the gui

--- client/src/elements/guiElements/gui.cpp
+++ client/src/elements/guiElements/gui.cpp
@@ -7,51 +7,86 @@
 
 #include "gui.hpp"
 
-RGui::RGui()
-{
-    this->AddButton(GButton(Vector2{150, 150}, Vector2{100, 50}, "Button"));
-    this->AddWindBox(GWindBox(Vector2{300, 300}, Vector2{100, 50}, "WindBox"));
-    this->_buttons[0].setAction([this]() { this->_windBoxes[0].setState(true); });
-    this->AddText(RaylibText("Hello World", Vector2{400, 400}, 20, WHITE));
-    this->AddCheckBox(GCheckBox(Vector2{500, 500}, Vector2{100, 50}, "CheckBox"));
-    this->AddSlider(GSlider(Vector2{300, 400}, Vector2{100, 50}, "Slider", 50, 0, 100));
-    this->AddList(GList(Vector2{300, 500}, Vector2{100, 50}, "List;elem1;elem2;elem3"));
-    this->AddListEx(GListEx(Vector2{500, 0}, Vector2{100, 200}, {"List", "Ex", "with", "many", "element", "to", "test", "that"}, 0));
-    this->addInputText(GTextInput(Vector2{500, 300}, Vector2{100, 50}, "enter text"));
-    this->addInputTextBox(GTextInputBox(Vector2{500, 400}, Vector2{200, 100}, "InputText", "Enter text:", "OK;Cancel", "default text", 100));
-}
+// RGui::RGui()
+// {
+//     // this->AddButton(GButton(Vector2{150, 150}, Vector2{100, 50}, "Button"));
+//     // this->AddWindBox(GWindBox(Vector2{300, 300}, Vector2{100, 50}, "WindBox"));
+//     // this->_buttons[0].setAction([this]() { this->_windBoxes[0].setState(true); });
+//     // this->AddText(RaylibText("Hello World", Vector2{400, 400}, 20, WHITE));
+//     // this->AddCheckBox(GCheckBox(Vector2{500, 500}, Vector2{100, 50}, "CheckBox"));
+//     // this->AddSlider(GSlider(Vector2{300, 400}, Vector2{100, 50}, "Slider", 50, 0, 100));
+//     // this->AddList(GList(Vector2{300, 500}, Vector2{100, 50}, "List;elem1;elem2;elem3"));
+//     // this->AddListEx(GListEx(Vector2{500, 0}, Vector2{100, 200}, {"List", "Ex", "with", "many", "element", "to", "test", "that"}, 0));
+//     // this->addInputText(GTextInput(Vector2{500, 300}, Vector2{100, 50}, "enter text"));
+//     // this->addInputTextBox(GTextInputBox(Vector2{500, 400}, Vector2{200, 100}, "InputText", "Enter text:", "OK;Cancel", "default text", 100));
+//     // this->addSpinner(GSpinner(Vector2{400, 400}, Vector2{100, 50}, "Spinner", 0, 0, 100));
+//     // this->AddValueBox(GValueBox(Vector2{400, 500}, Vector2{100, 50}, "ValueBox", 0, 0, 100));
+//     // this->AddGroup(GGroup(Vector2{0, 0}, Vector2{100, 100}, "Group"));
+//     // this->AddToggleGroup(GToggleGroup(Vector2{200, 200}, Vector2{100, 100}, "elem1;elem2;elem3"));
+//     //this->AddToggleSlider(GToggleSlider(Vector2{400, 400}, Vector2{100, 50}, "elem1;elem2", 0));
+//     // this->AddPannel(GPannel(Vector2{400, 400}, Vector2{200, 200}, "Pannel"));
+//     // this->AddColorPicker(GColorPicker(Vector2{450, 450}, Vector2{100, 100}, "ColorPicker", Color{0, 0, 0, 255}));
+//     //this->AddProgressBar(GProgressBar(Vector2{400, 400}, Vector2{100, 50}, "ProgressBar", "pwogwesbaw", 50, 0, 100));
+// }
 
 
 // ----------------------- DRAW GUI -----------------------
 void RGui::DrawGui() const
 {
-    if (!_buttons.empty())
-        for (const auto &button : _buttons)
+    if (!_Buttons.empty())
+        for (const auto &button : _Buttons)
             button.DrawRlibButton();
-    if (!_windBoxes.empty())
-        for (const auto &windBox : _windBoxes)
+    if (!_WindBoxes.empty())
+        for (const auto &windBox : _WindBoxes)
             windBox.DrawRlibWindBox();
-    if (!_texts.empty())
-        for (const auto &text : _texts)
+    if (!_Texts.empty())
+        for (const auto &text : _Texts)
             text.DrawRlibText();
-    if (!_checkBoxes.empty())
-        for (const auto &checkBox : _checkBoxes)
+    if (!_CheckBoxes.empty())
+        for (const auto &checkBox : _CheckBoxes)
             checkBox.DrawRlibCheckBox();
-    if (!_sliders.empty())
-        for (const auto &slider : _sliders)
+    if (!_Sliders.empty())
+        for (const auto &slider : _Sliders)
             slider.DrawRlibSlider();
-    if (!_lists.empty())
-        for (const auto &list : _lists)
+    if (!_Lists.empty())
+        for (const auto &list : _Lists)
             list.DrawRlibList();
-    if (!_listExs.empty())
-        for (const auto &listEx : _listExs)
+    if (!_ListExs.empty())
+        for (const auto &listEx : _ListExs)
             listEx.DrawRlibListEx();
-    if (!_inputTexts.empty())
-        for (const auto &inputText : _inputTexts)
+    if (!_InputTexts.empty())
+        for (const auto &inputText : _InputTexts)
             inputText.DrawRlibTextInput();
-    if (!_inputTextsBoxs.empty())
-        for (const auto &inputText : _inputTextsBoxs)
+    if (!_InputTextsBoxs.empty())
+        for (const auto &inputText : _InputTextsBoxs)
             inputText.DrawRlibTextInputBox();
+    if (!_Spinners.empty())
+        for (const auto &spinner : _Spinners)
+            spinner.DrawRlibSpinner();
+    if (!_ValueBoxes.empty())
+        for (const auto &valueBox : _ValueBoxes)
+            valueBox.DrawRlibValueBox();
+    if (!_Groups.empty())
+        for (const auto &group : _Groups)
+            group.DrawRlibGroup();
+    if (!_ToggleGroups.empty())
+        for (const auto &toggleGroup : _ToggleGroups)
+            toggleGroup.DrawRlibToggleGroup();
+    if (!_ToggleSliders.empty())
+        for (const auto &toggleSlider : _ToggleSliders)
+            toggleSlider.DrawRlibToggleSlider();
+    if (!_Pannels.empty())
+        for (const auto &pannel : _Pannels)
+            pannel.DrawRlibPannel();
+    if (!_ColorPickers.empty())
+        for (const auto &colorPicker : _ColorPickers)
+            colorPicker.DrawRlibColorPicker();
+    if (!_ProgressBars.empty())
+        for (const auto &progressBar : _ProgressBars)
+            progressBar.DrawRlibProgressBar();
+    if (!_DropDowns.empty())
+        for (const auto &dropDown : _DropDowns)
+            dropDown.DrawRlibDropDown();
 }
 
 
@@ -78,52 +113,93 @@ void RGui::DrawGui() const
 
 void RGui::AddButton(GButton button)
 {
-    _buttons.push_back(button);
+    _Buttons.push_back(button);
 }
 
 void RGui::AddWindBox(GWindBox windBox)
 {
-    _windBoxes.push_back(windBox);
+    _WindBoxes.push_back(windBox);
 }
 
 void RGui::AddText(RaylibText text)
 {
-    _texts.push_back(text);
+    _Texts.push_back(text);
 }
 
 void RGui::AddCheckBox(GCheckBox checkBox)
 {
-    _checkBoxes.push_back(checkBox);
+    _CheckBoxes.push_back(checkBox);
 }
 
 void RGui::AddSlider(GSlider slider)
 {
-    _sliders.push_back(slider);
+    _Sliders.push_back(slider);
 }
 
 void RGui::AddList(GList list)
 {
-    _lists.push_back(list);
+    _Lists.push_back(list);
 }
 
 void RGui::AddListEx(GListEx listEx)
 {
-    _listExs.push_back(listEx);
+    _ListExs.push_back(listEx);
 }
 
 void RGui::addInputText(GTextInput inputText)
 {
-    _inputTexts.push_back(inputText);
+    _InputTexts.push_back(inputText);
 }
 
 void RGui::addInputTextBox(GTextInputBox inputText)
 {
-    _inputTextsBoxs.push_back(inputText);
+    _InputTextsBoxs.push_back(inputText);
 }
 
+void RGui::addSpinner(GSpinner spinner)
+{
+    _Spinners.push_back(spinner);
+}
 
+void RGui::AddValueBox(GValueBox valueBox)
+{
+    _ValueBoxes.push_back(valueBox);
+}
 
+void RGui::AddGroup(GGroup group)
+{
+    _Groups.push_back(group);
+}
 
+void RGui::AddToggleGroup(GToggleGroup toggleGroup)
+{
+    _ToggleGroups.push_back(toggleGroup);
+}
+
+void RGui::AddToggleSlider(GToggleSlider toggleSlider)
+{
+    _ToggleSliders.push_back(toggleSlider);
+}
+
+void RGui::AddPannel(GPannel pannel)
+{
+    _Pannels.push_back(pannel);
+}
+
+void RGui::AddColorPicker(GColorPicker colorPicker)
+{
+    _ColorPickers.push_back(colorPicker);
+}
+
+void RGui::AddProgressBar(GProgressBar progressBar)
+{
+    _ProgressBars.push_back(progressBar);
+}
+
+void RGui::AddDropDown(GDropDown dropDown)
+{
+    _DropDowns.push_back(dropDown);
+}
 
 
 
@@ -145,45 +221,90 @@ void RGui::addInputTextBox(GTextInputBox inputText)
 
 void RGui::SetButtons(std::vector<GButton> buttons)
 {
-    _buttons = buttons;
+    _Buttons = buttons;
 }
 
 void RGui::SetWindBoxes(std::vector<GWindBox> windBoxes)
 {
-    _windBoxes = windBoxes;
+    _WindBoxes = windBoxes;
 }
 
 void RGui::setTexts(std::vector<RaylibText> texts)
 {
-    _texts = texts;
+    _Texts = texts;
 }
 
 void RGui::setCheckBoxes(std::vector<GCheckBox> checkBoxes)
 {
-    _checkBoxes = checkBoxes;
+    _CheckBoxes = checkBoxes;
 }
 
 void RGui::setSliders(std::vector<GSlider> sliders)
 {
-    _sliders = sliders;
+    _Sliders = sliders;
 }
 
 void RGui::setLists(std::vector<GList> lists)
 {
-    _lists = lists;
+    _Lists = lists;
 }
 
 void RGui::setListExs(std::vector<GListEx> listExs)
 {
-    _listExs = listExs;
+    _ListExs = listExs;
 }
 
 void RGui::setInputTexts(std::vector<GTextInput> inputTexts)
 {
-    _inputTexts = inputTexts;
+    _InputTexts = inputTexts;
 }
 
 void RGui::setInputTextsBoxs(std::vector<GTextInputBox> inputTexts)
 {
-    _inputTextsBoxs = inputTexts;
+    _InputTextsBoxs = inputTexts;
+}
+
+void RGui::setSpinners(std::vector<GSpinner> spinners)
+{
+    _Spinners = spinners;
+}
+
+void RGui::setValueBoxes(std::vector<GValueBox> valueBoxes)
+{
+    _ValueBoxes = valueBoxes;
+}
+
+void RGui::setGroups(std::vector<GGroup> groups)
+{
+    _Groups = groups;
+}
+
+void RGui::setToggleGroups(std::vector<GToggleGroup> toggleGroups)
+{
+    _ToggleGroups = toggleGroups;
+}
+
+void RGui::setToggleSliders(std::vector<GToggleSlider> toggleSliders)
+{
+    _ToggleSliders = toggleSliders;
+}
+
+void RGui::setPannels(std::vector<GPannel> pannels)
+{
+    _Pannels = pannels;
+}
+
+void RGui::setColorPickers(std::vector<GColorPicker> colorPickers)
+{
+    _ColorPickers = colorPickers;
+}
+
+void RGui::setProgressBars(std::vector<GProgressBar> progressBars)
+{
+    _ProgressBars = progressBars;
+}
+
+void RGui::setDropDowns(std::vector<GDropDown> dropDowns)
+{
+    _DropDowns = dropDowns;
 }
--- client/src/elements/guiElements/gui.hpp
+++ client/src/elements/guiElements/gui.hpp
@@ -12,7 +12,7 @@
 
 class RGui {
 public:
-    RGui();
+    RGui() = default;
     ~RGui() = default;
 
     void DrawGui() const;
@@ -26,6 +26,15 @@ public:
     void AddListEx(GListEx listEx);
     void addInputText(GTextInput inputText);
     void addInputTextBox(GTextInputBox inputText);
+    void addSpinner(GSpinner spinner);
+    void AddValueBox(GValueBox valueBox);
+    void AddGroup(GGroup group);
+    void AddToggleGroup(GToggleGroup toggleGroup);
+    void AddToggleSlider(GToggleSlider toggleSlider);
+    void AddPannel(GPannel pannel);
+    void AddColorPicker(GColorPicker colorPicker);
+    void AddProgressBar(GProgressBar progressBar);
+    void AddDropDown(GDropDown dropDown);
 
     void SetButtons(std::vector<GButton> buttons);
     void SetWindBoxes(std::vector<GWindBox> windBoxes);
@@ -36,14 +45,32 @@ public:
     void setListExs(std::vector<GListEx> listExs);
     void setInputTexts(std::vector<GTextInput> inputTexts);
     void setInputTextsBoxs(std::vector<GTextInputBox> inputTexts);
+    void setSpinners(std::vector<GSpinner> spinners);
+    void setValueBoxes(std::vector<GValueBox> valueBoxes);
+    void setGroups(std::vector<GGroup> groups);
+    void setToggleGroups(std::vector<GToggleGroup> toggleGroups);
+    void setToggleSliders(std::vector<GToggleSlider> toggleSliders);
+    void setPannels(std::vector<GPannel> pannels);
+    void setColorPickers(std::vector<GColorPicker> colorPickers);
+    void setProgressBars(std::vector<GProgressBar> progressBars);
+    void setDropDowns(std::vector<GDropDown> dropDowns);
 private:
-    std::vector<GButton> _buttons;
-    std::vector<GWindBox> _windBoxes;
-    std::vector<RaylibText> _texts;
-    std::vector<GCheckBox> _checkBoxes;
-    std::vector<GSlider> _sliders;
-    std::vector<GList> _lists;
-    std::vector<GListEx> _listExs;
-    std::vector<GTextInput> _inputTexts;
-    std::vector<GTextInputBox> _inputTextsBoxs;
+    std::vector<GButton> _Buttons;
+    std::vector<GWindBox> _WindBoxes;
+    std::vector<RaylibText> _Texts;
+    std::vector<GCheckBox> _CheckBoxes;
+    std::vector<GSlider> _Sliders;
+    std::vector<GList> _Lists;
+    std::vector<GListEx> _ListExs;
+    std::vector<GTextInput> _InputTexts;
+    std::vector<GTextInputBox> _InputTextsBoxs;
+    std::vector<GSpinner> _Spinners;
+    std::vector<GValueBox> _ValueBoxes;
+    std::vector<GGroup> _Groups;
+    std::vector<GToggleGroup> _ToggleGroups;
+    std::vector<GToggleSlider> _ToggleSliders;
+    std::vector<GPannel> _Pannels;
+    std::vector<GColorPicker> _ColorPickers;
+    std::vector<GProgressBar> _ProgressBars;
+    std::vector<GDropDown> _DropDowns;
 };
