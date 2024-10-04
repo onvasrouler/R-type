a790e1f852d5664a9eb73e5fd8e31657991ab181
julien <julien.renard@epitech.eu>
2024-10-04 21:38:08 +0200

    :building_construction: move "move_entity" function to "move" in IEntity Interface

--- server/src/entity/IEntity.hpp
+++ server/src/entity/IEntity.hpp
@@ -9,23 +9,23 @@
 
 #include <memory>
  
- class IEntity
- {
- public:
-    virtual ~IEntity() = default;
+class IEntity
+{
+public:
+   virtual ~IEntity() = default;
 
-    virtual void move_entity() = 0;
+   virtual void move() = 0;
+    
+   virtual void set_l(int l) = 0;
+   virtual void set_h(int h) = 0;
+   virtual void set_x(int x) = 0;
+   virtual void set_y(int y) = 0;
+   virtual void set_type(int type) = 0;
 
-    virtual void set_l(int l) = 0;
-    virtual void set_h(int h) = 0;
-    virtual void set_x(int x) = 0;
-    virtual void set_y(int y) = 0;
-    virtual void set_type(int type) = 0;
-
-    virtual int get_x(void) = 0;
-    virtual int get_y(void) = 0;
-    virtual int get_l(void) = 0;
-    virtual int get_h(void) = 0;
-    virtual int get_id(void) = 0;
-    virtual int get_type(void) = 0;
- };
+   virtual int get_x(void) = 0;
+   virtual int get_y(void) = 0;
+   virtual int get_l(void) = 0;
+   virtual int get_h(void) = 0;
+   virtual int get_id(void) = 0;
+   virtual int get_type(void) = 0;
+};
