57f34627099d611806245362bc8b105a028363e2
mxncp85 <maxence.perronie@epitech.eu>
2024-10-07 16:24:36 +0200

    :heavy_plus_sign: Added back the main.cpp for the server

---
+++ server/game_engine/src/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** R-Type
+** File description:
+** Main
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, R-type game engine!" << std::endl;
+    return 0;
+}
