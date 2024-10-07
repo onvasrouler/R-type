5f5738491ab2d2015dd39736111cb16ecb3dce5b
mxncp85 <maxence.perronie@epitech.eu>
2024-10-07 14:04:57 +0200

    :wrench: Added a const int the ITools setter, and deleted the const of the getter

--- server/game_engine/src/tools/ITools.hpp
+++ server/game_engine/src/tools/ITools.hpp
@@ -12,7 +12,7 @@ class ITools
 public:
     virtual ~ITools() = default;
 
-    virtual void set_value(int value) = 0;
+    virtual void set_value(const int value) = 0;
 
-    virtual int get_value() const = 0;
+    virtual int get_value() = 0;
 };
