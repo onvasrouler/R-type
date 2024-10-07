cd1f7809aeb6352a36b22a9936cda95dd17ea1be
mxncp85 <maxence.perronie@epitech.eu>
2024-10-07 12:51:14 +0200

    :wrench: Added const to AEntity setters

--- server/game_engine/src/entity/AEntity.cpp
+++ server/game_engine/src/entity/AEntity.cpp
@@ -15,56 +15,56 @@ AEntity::AEntity()
 
 int AEntity::get_x() const
 {
-    return (this->_origin.get_x());
+    return this->_origin.get_x();
 }
 
 int AEntity::get_y() const
 {
-    return (this->_origin.get_y());
+    return this->_origin.get_y();
 }
 
 int AEntity::get_l() const
 {
-    return (this->_dimension.get_l());
+    return this->_dimension.get_l();
 }
 
 int AEntity::get_h() const
 {
-    return (this->_dimension.get_h());
+    return this->_dimension.get_h();
 }
 
 int AEntity::get_type() const
 {
-    return (this->_type.get_value());
+    return this->_type.get_value();
 }
 
 int AEntity::get_id() const
 {
-    return (this->_id.get_value());
+    return this->_id.get_value();
 }
 
 
-void AEntity::set_x(int x)
+void AEntity::set_x(const int x)
 {
     this->_origin.set_x(x);
 }
 
-void AEntity::set_y(int y)
+void AEntity::set_y(const int y)
 {
     this->_origin.set_y(y);
 }
 
-void AEntity::set_l(int l)
+void AEntity::set_l(const int l)
 {
     this->_dimension.set_l(l);
 }
 
-void AEntity::set_h(int h)
+void AEntity::set_h(const int h)
 {
     this->_dimension.set_h(h);
 }
 
-void AEntity::set_type(int type)
+void AEntity::set_type(const int type)
 {
     this->_dimension.set_h(type);
 }
