3789711af0837079906155be9aa5bb6ea588fb03
Antoinegtir <antoine.gonthier@epitech.eu>
2024-10-13 21:40:44 +0200

    :wrench: add branch dev

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ VCPKG_REPO="https://github.com/microsoft/vcpkg.git"
 VCPKG_DIR="$HOME/vcpkg"
 REPO_URL="git@github.com:onvasrouler/R-type.git"
 REPO_DIR="r-types"
-BRANCH="r-type-installer"
+BRANCH="dev"
 PLATFORM=$(uname)
 
 rm -rf build
