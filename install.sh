8362585277a644f2b0528767176cf82d158368a5
Antoine <antoine.gonthier@epitech.eu>
2024-10-13 19:43:16 +0200

    :wrench: update branch name

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ VCPKG_REPO="https://github.com/microsoft/vcpkg.git"
 VCPKG_DIR="$HOME/vcpkg"
 REPO_URL="git@github.com:onvasrouler/R-type.git"
 REPO_DIR="r-types"
-BRANCH="dev"
+BRANCH="r-type-installer"
 PLATFORM=$(uname)
 
 rm -rf build
