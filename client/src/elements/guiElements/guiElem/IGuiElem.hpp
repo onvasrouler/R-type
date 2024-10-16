957f697df9aa50daab6580b24fe0ef7b0b693ab1
aimericdrk <aimeric.rouyer@epitech.eu>
2024-10-17 00:45:47 +0200

    :arrow_up: new feature to Igui so you can now toggle display state and set and get the zindex

--- client/src/elements/guiElements/guiElem/IGuiElem.hpp
+++ client/src/elements/guiElements/guiElem/IGuiElem.hpp
@@ -18,7 +18,9 @@ public:
     virtual void setSize(const Vector2 size) = 0;
     virtual void setValue(const std::string value) = 0;
     virtual void setId(const std::string id) = 0;
+    virtual void toggleDisplay() = 0;
     virtual void setDisplay(const bool state) = 0;
+    virtual void setZIndex(const int zIndex) = 0;
 
     virtual void draw() const = 0;
 
@@ -28,4 +30,5 @@ public:
     virtual std::string getValue() const = 0;
     virtual std::string getId() const = 0;
     virtual bool getDisplay() const = 0;
+    virtual int getZIndex() const = 0;
 };
