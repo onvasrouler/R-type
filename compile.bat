4dadce577c059c76a8383b15a2234727cd05ccf0
ciremiadrk <aimeric.rouyer@epitech.eu>
2024-10-22 15:54:28 +0200

    :white_check_mark: adding a new compiler

---
+++ compile.bat
@@ -0,0 +1,81 @@
+@echo off
+setlocal
+
+set TOOLCHAIN_FILE_PATH=%USERPROFILE%\vcpkg\scripts\buildsystems\vcpkg.cmake
+
+echo "dcmake_toolchain_file: %TOOLCHAIN_FILE_PATH%"
+
+rem Clean up previous builds
+if exist build (
+    rmdir /s /q build
+)
+if exist r-type_server (
+    del r-type_server
+)
+if exist r-type_client (
+    del r-type_client
+)
+if exist r-type_tests (
+    del r-type_tests
+)
+
+rem Detect operating system
+set "OS=%OS%"
+if "%OS%"=="Windows_NT" (
+    echo Running on Windows
+) else (
+    echo Running on Linux
+)
+cmake --build . --target clean
+if "%~1"=="clean" (
+    exit /b
+) else if "%~1"=="tests" (
+    echo Building tests
+    cmake -S . -B build -DTESTS=ON -DSERVER=ON -DCLIENT=ON -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE_PATH% -Wno-dev -D_WIN32_WINNT=0x0601
+    cmake --build build --config Release
+    start "" "build/R-type.sln"
+    cd build
+    if "%OS%"=="Windows_NT" (
+        echo Running tests on Windows
+        build\tests\r-type_tests.exe
+    ) else (
+        echo Running tests on Linux
+        build/tests/r-type_tests
+    )
+) else if "%~1"=="server" (
+    echo Building server
+    rmdir /s /q serverModules
+    cmake -S . -B build -DTESTS=OFF -DSERVER=ON -DCLIENT=OFF -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE_PATH% -Wno-dev -D_WIN32_WINNT=0x0601
+    cd build
+    cmake --build .
+    cd ..
+    move /Y build\server\Debug\r-type_server.exe .
+    mkdir serverModules
+    move /Y build\server\modules\gameModule\Debug\gameModule.dll serverModules
+    move /Y build\server\modules\networkModule\Debug\networkModule.dll serverModules
+) else if "%~1"=="client" (
+    echo Building client
+    cmake -S . -B build -DTESTS=OFF -DSERVER=OFF -DCLIENT=ON -DCMAKE_TOOLCHAIN_FILE=C:/Users/aimer/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_PREFIX_PATH=C:/Users/aimer/vcpkg/installed/x64-windows -Wno-dev -D_WIN32_WINNT=0x0601 > ../build_log.txt 2>&1
+    IF ERRORLEVEL 1 (
+        echo CMake configuration failed! See build_log.txt for details.
+        exit /b 1
+    )
+    cd build
+    cmake --build .  > ../build_log.txt 2>&1
+    IF ERRORLEVEL 1 (
+        echo Build failed! See build_log.txt for details.
+        exit /b 1
+    )
+    echo "Client built!"
+    echo "Client built !"
+) else (
+    echo Building server and client
+    cmake -S . -B build -DTESTS=OFF -DSERVER=ON -DCLIENT=ON -DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE_PATH% -Wno-dev -D_WIN32_WINNT=0x0601
+    cd build
+    cmake --build .
+    cd ..
+    move /Y build/server/r-type_server .
+    move /Y build/client/r-type_client .
+)
+
+endlocal
