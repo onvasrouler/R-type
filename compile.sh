d0538f2bf0fc0385b24f7079762e850e172f2d09
Quentin Tennerel <quentin.tennerel@epitech.eu>
2024-10-23 15:50:14 +0200

    🔨 make the compile.sh script more clean

--- compile.sh
+++ compile.sh
@@ -1,70 +1,109 @@
 #!/bin/bash
 
-rm -rf build
-rm -rf serverModules
-rm -f r-type_server
-rm -f r-type_client
-rm -f r-type_tests
-if [ "$1" == "clean" ]; then
-    rm -rf build
-    rm -rf serverModules
-    rm -f r-type_server
-    rm -f r-type_client
-    rm -f r-type_tests
-    rm -rf server_dev_tools/serverModules
-    rm -f server_dev_tools/r-type_server
+server_binary="r-type_server"
+client_binary="r-type_client"
+tests_binary="r-type_tests"
+serverModulesDir="serverModules"
+
+clean_server() {
+    rm -f $server_binary
+    rm -f $serverModulesDir/*
     rm -f server_dev_tools/server/config/unix/*.a
-else if [ "$1" == "tests" ]; then
-    cmake -S . -B build -DTESTS=ON
-    cd build
-    make
-    cd ..
-    sudo ./build/tests/r-type_tests
-else if [ "$1" == "server" ]; then
-    rm -f serverModules/*
-    rm -f server_dev_tools/server/config/unix/*.a
-    rm -rf server_dev_tools/serverModules/gameModule.so server_dev_tools/serverModules/networkModule.so server_dev_tools/serverModules/logModule.so
-    rm -f server_dev_tools/r-type_server
+    rm -rf server_dev_tools/$serverModulesDir/gameModule.so server_dev_tools/$serverModulesDir/networkModule.so server_dev_tools/$serverModulesDir/logModule.so
+    rm -f server_dev_tools/$server_binary
     rm -f server_dev_tools/source_code/*.cpp
     rm -f server_dev_tools/source_code/*.hpp
+}
+
+clean_client() {
+    rm -f $client_binary
+}
+
+clean_tests() {
+    rm -f $tests_binary
+}
+
+clean_all() {
+    clean_server
+    clean_client
+    clean_tests
+}
+
+copy_server_source_code() {
     cp server/modules/MultiThread.* server_dev_tools/source_code
     cp server/modules/AbstractModule.* server_dev_tools/source_code
     cp server/modules/UUID.* server_dev_tools/source_code
-    cmake -S . -B build -DTESTS=OFF -DSERVER=ON -DCLIENT=OFF
-    cd build
-    make
-    cd ..
-    mv build/server/r-type_server .
+}
+
+move_server_and_modules() {
+    mv build/server/$server_binary .
     mv build/server/modules/gameModule/*.so .
     mv build/server/modules/networkModule/*.so .
     mv build/server/modules/logModule/*.so .
     mv libgameModule.so gameModule.so
     mv libnetworkModule.so networkModule.so
     mv liblogModule.so logModule.so
+    mkdir $serverModulesDir
+    mv gameModule.so $serverModulesDir
+    mv networkModule.so $serverModulesDir
+    mv logModule.so $serverModulesDir
+}
+
+setup_dev_tools() {
     mv build/server_dev_tools/source_code/libmoduleSourceCode.a server_dev_tools/server/config/unix
     mv server_dev_tools/server/config/unix/libmoduleSourceCode.a server_dev_tools/server/config/unix/moduleSourceCode.a
-    mkdir serverModules
-    mv gameModule.so serverModules
-    mv networkModule.so serverModules
-    mv logModule.so serverModules
-    mkdir server_dev_tools/serverModules
-    cp serverModules/*.so server_dev_tools/serverModules
-    cp r-type_server server_dev_tools
-    chmod 444 server_dev_tools/serverModules/*.so
-else if [ "$1" == "client" ]; then
+    mkdir server_dev_tools/$serverModulesDir
+    cp $serverModulesDir/*.so server_dev_tools/$serverModulesDir
+    cp $server_binary server_dev_tools
+    chmod 444 server_dev_tools/$serverModulesDir/*.so
+}
+
+compile_server() {
+    clean_server
+    copy_server_source_code
+    cmake -S . -B build -DTESTS=OFF -DSERVER=ON -DCLIENT=OFF
+    cd build
+    make
+    cd ..
+    move_server_and_modules
+    setup_dev_tools
+}
+
+compile_client() {
     cmake -S . -B build -DTESTS=OFF -DSERVER=OFF -DCLIENT=ON
     cd build
     make
     cd ..
-    mv build/client/r-type_client .
-else
+    mv build/client/$client_binary .
+}
+
+compile_tests () {
+    clean_all
+    cmake -S . -B build -DTESTS=ON -DSERVER=ON -DCLIENT=ON
+    cd build
+    make
+    cd ..
+    sudo ./build/tests/$tests_binary
+}
+
+compile() {
     cmake -S . -B build -DTESTS=OFF -DSERVER=ON -DCLIENT=ON
     cd build
     make
     cd ..
-    mv build/server/r-type_server .
-    mv build/client/r-type_client .
-fi
-fi
-fi
+    mv build/server/$server_binary .
+    mv build/client/$client_binary .
+}
+
+rm -rf build
+if [ "$1" == "clean" ]; then
+    clean_all
+elif [ "$1" == "tests" ]; then
+    compile_tests
+elif [ "$1" == "server" ]; then
+    compile_server
+elif [ "$1" == "client" ]; then
+    compile_client
+else
+    compile
 fi
