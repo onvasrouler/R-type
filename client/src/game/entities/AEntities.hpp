12d63786271e2073c22a45abe2cefbda504d6108
aimericdrk <aimeric.rouyer@epitech.eu>
2024-10-29 18:45:35 +0100

    :sparkles: create the Entity with the abstract and the Interface

---
+++ client/src/game/entities/AEntities.hpp
@@ -0,0 +1,66 @@
+/*
+** EPITECH PROJECT, 2024
+** R-type-2
+** File description:
+** AEntities
+*/
+
+#pragma once
+
+#include "IEntities.hpp"
+
+class AEntities : public IEntities {
+public:
+    AEntities();
+    virtual ~AEntities() = default;
+
+    void setPos(const Vector2 pos) override;
+    void setPosX(const float x) override;
+    void setPosY(const float y) override;
+    void setSpeed(const Vector2 speed) override;
+    void setSpeedX(const float x) override;
+    void setSpeedY(const float y) override;
+    void setHealth(const int health) override;
+    void setDamage(const int damage) override;
+    void setAlive(const bool alive) override;
+
+    void setTexturePath(const std::string texture) override;
+    void setTexture(const Texture2D texture) override;
+    void setSize(const Vector2 size) override;
+    void setScale(const float scale) override;
+    void setRotation(const float rotation) override;
+
+    void setId(const std::string id) override;
+
+    void draw() const override;
+
+    Vector2 getPos() const override;
+    float getPosX() const override;
+    float getPosY() const override;
+    Vector2 getSpeed() const override;
+    float getSpeedX() const override;
+    float getSpeedY() const override;
+    int getHealth() const override;
+    int getDamage() const override;
+    bool getAlive() const override;
+
+    std::string getTexturePath() const override;
+    Texture2D getTexture() const override;
+    Vector2 getSize() const override;
+    float getScale() const override;
+    float getRotation() const override;
+
+    std::string getId() const override;
+protected:
+    Vector2 _Pos;
+    Vector2 _Speed;
+    int _Health;
+    int _Damage;
+    bool _Alive;
+    std::string _TexturePath;
+    Texture2D _Texture;
+    Vector2 _Size;
+    float _Scale;
+    float _Rotation;
+    std::string _Id;
+};
---
+++ client/src/game/entities/IEntities.hpp
@@ -0,0 +1,54 @@
+/*
+** EPITECH PROJECT, 2024
+** R-type-2
+** File description:
+** IEntities
+*/
+
+#pragma once
+
+#include <raylib.h>
+#include <string>
+
+class IEntities {
+public:
+    virtual ~IEntities() = default;
+
+    virtual void setPos(const Vector2 pos) = 0;
+    virtual void setPosX(const float x) = 0;
+    virtual void setPosY(const float y) = 0;
+    virtual void setSpeed(const Vector2 speed) = 0;
+    virtual void setSpeedX(const float x) = 0;
+    virtual void setSpeedY(const float y) = 0;
+    virtual void setHealth(const int health) = 0;
+    virtual void setDamage(const int damage) = 0;
+    virtual void setAlive(const bool alive) = 0;
+
+    virtual void setTexturePath(const std::string texture) = 0;
+    virtual void setTexture(const Texture2D texture) = 0;
+    virtual void setSize(const Vector2 size) = 0;
+    virtual void setScale(const float scale) = 0;
+    virtual void setRotation(const float rotation) = 0;
+
+    virtual void setId(const std::string id) = 0;
+
+    virtual void draw() const = 0;
+
+    virtual Vector2 getPos() const = 0;
+    virtual float getPosX() const = 0;
+    virtual float getPosY() const = 0;
+    virtual Vector2 getSpeed() const = 0;
+    virtual float getSpeedX() const = 0;
+    virtual float getSpeedY() const = 0;
+    virtual int getHealth() const = 0;
+    virtual int getDamage() const = 0;
+    virtual bool getAlive() const = 0;
+
+    virtual std::string getTexturePath() const = 0;
+    virtual Texture2D getTexture() const = 0;
+    virtual Vector2 getSize() const = 0;
+    virtual float getScale() const = 0;
+    virtual float getRotation() const = 0;
+
+    virtual std::string getId() const = 0;
+};
