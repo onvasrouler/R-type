9d20469935867cb2363c1ded513eaa3c3a8df0f2
aimericdrk <aimeric.rouyer@epitech.eu>
2024-10-29 18:43:11 +0100

    :sparkles:  create the game element for the moment it is only used to create the entities

---
+++ client/src/game/game.hpp
@@ -0,0 +1,42 @@
+/*
+** EPITECH PROJECT, 2024
+** R-type-2
+** File description:
+** game
+*/
+
+#pragma once
+#include "entities/EntitiesFactory.hpp"
+
+#define ASSETS_FILE_PATH "./assets/assets_settings.json"
+
+class Game {
+public:
+    Game(std::shared_ptr<JsonParser> jsonParser = nullptr, std::shared_ptr<DebugLogger> debugLogger = nullptr);
+    ~Game() = default;
+
+    void setEntityFactoryJsonParser(std::shared_ptr<JsonParser> jsonParser);
+
+    void setServerInfos(const std::string ip, const std::string port);
+    void setPort(const std::string port);
+    void setIp(const std::string ip);
+
+    void initGame();
+
+    void run();
+
+    void stop();
+
+    bool isRunning() const;
+    std::shared_ptr<EntitiesFactory> getEntitiesFactory() const;
+    std::string getIp() const;
+    std::string getPort() const;
+
+private:
+    std::shared_ptr<EntitiesFactory> _EntitiesFactory; /**< Shared pointer to a EntitiesFactory object. */
+    std::shared_ptr<DebugLogger> _DebugLogger; /**< Shared pointer to a DebugLogger object. */
+    bool _Running; /**< Boolean to check if the game is running. */
+
+    std::string _Ip;
+    std::string _Port;
+};
