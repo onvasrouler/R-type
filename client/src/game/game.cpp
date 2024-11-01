5ab4eb99170e7c8a012934493d8d84ea0acc6164
aimericdrk <aimeric.rouyer@epitech.eu>
2024-11-01 19:45:15 +0100

    :sparkles: creating the actual game and adding the possibility to relaunch game

--- client/src/game/game.cpp
+++ client/src/game/game.cpp
@@ -16,6 +16,7 @@ Game::Game(std::shared_ptr<JsonParser> jsonParser, std::shared_ptr<DebugLogger> 
     _JsonParser = jsonParser;
     _EntitiesFactory = std::make_shared<EntitiesFactory>(jsonParser, debugLogger);
     _Running = false;
+    _GameOver = false;
 }
 
 void Game::setEntityFactoryJsonParser(std::shared_ptr<JsonParser> jsonParser)
@@ -47,7 +48,11 @@ void Game::setWindowSize(const int width, const int height)
 
 void Game::initGame()
 {
-    _EntitiesFactory->loadEntities(ASSETS_FILE_PATH);
+    if (_EntitiesFactory)
+        _EntitiesFactory->loadEntities(ASSETS_FILE_PATH);
+    else
+        if (_DebugLogger)
+            _DebugLogger->Log("EntitiesFactory not setted", 0);
     if (_DebugLogger)
         _DebugLogger->Log("Game is initialized", 2);
 }
@@ -55,6 +60,7 @@ void Game::initGame()
 void Game::start()
 {
     _Running = true;
+    _ShutDown = false;
     if (_DebugLogger)
         _DebugLogger->Log("Game is running", 2);
 }
@@ -65,28 +71,215 @@ void Game::update(std::string data)
         return;
     if (_DebugLogger)
         _DebugLogger->Log("Game received data", 4);
-    _DataBuffer = data;
+    for (char c : data) {
+        if (!isalnum(c) && c != '/' && c != '-' && c != '\n' && c != '\r') {
+            if (_DebugLogger)
+                _DebugLogger->Log("Invalid character in data", 0);
+            return;
+        }
+    }
+    if (data == "500\r\n") {
+        _ShutDown = true;
+        resetGame();
+    }
+    try {
+
+        std::vector<std::string> tokens = splitter(data, '/');
+        _DataBuffer = data;
+        handleData(tokens);
+
+    } catch (std::exception &e) {
+        if (_DebugLogger)
+            _DebugLogger->Log("Error while updating game", 0);
+    }
+}
+
+void Game::handleData(std::vector<std::string> tokens)
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is handling data", 4);
+    for (const auto& token : tokens) {
+        std::cout << token << std::endl;
+    }
+
+    try {
+        std::string instruction = tokens[0];
+        std::string uuid = tokens[1];
+    if (instruction == "01" || instruction == "200" || instruction == "201" )
+        handlePlayer(uuid, tokens);
+    else if (instruction == "210" || instruction == "211" ) 
+        handleEnemy(uuid, tokens);
+    else if (instruction == "220")
+        handleBullet(uuid, tokens);
+
+    if (instruction == "203" || instruction == "213" || instruction == "222")
+        destroyEntity(tokens);
+    
+    if (instruction == "202" || instruction == "212" || instruction == "221") {
+        if (_EntitiesList.find(uuid) != _EntitiesList.end()) {
+            _EntitiesList[uuid]->setRelativePosX(std::stoi(tokens[2]), _WindowWidth);
+            _EntitiesList[uuid]->setRelativePosY(std::stoi(tokens[3]), _WindowHeight);
+        } else {
+            std::cout << "creating entity bc id " << uuid << " not found" << std::endl;
+            #ifdef _WIN32
+            #else
+            if (uuid.length() != 36)
+                return;
+            #endif
+            if (tokens[0] == "202")
+                createPlayer(uuid);
+            if (tokens[0] == "212")
+                createEnemy(uuid);
+            if (tokens[0] == "221")
+                createBullet(uuid);
+            _EntitiesList[uuid]->setRelativePosX(std::stoi(tokens[2]), _WindowWidth);
+            _EntitiesList[uuid]->setRelativePosY(std::stoi(tokens[3]), _WindowHeight);
+        }
+    }
+    } catch (std::exception &e) {
+        if (_DebugLogger)
+            _DebugLogger->Log("Error while handling data", 0);
+    }
+        
+}
+
+void Game::handlePlayer(std::string uuid, std::vector<std::string> tokens)
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is handling player", 4);
+    if (tokens[0] == "01") {
+        createPlayer(uuid);
+        _EntitiesList[uuid]->setRelativePosX(std::stoi(tokens[5]), _WindowWidth);
+        _EntitiesList[uuid]->setRelativePosY(std::stoi(tokens[6]), _WindowHeight);
+        _PlayerId = uuid;
+    }
+    if (tokens[0] == "200") {
+        createPlayer(uuid);
+        _EntitiesList[uuid]->setRelativePosX(std::stoi(tokens[5]), _WindowWidth);
+        _EntitiesList[uuid]->setRelativePosY(std::stoi(tokens[6]), _WindowHeight);
+    }
+}
+
+void Game::handleEnemy(std::string uuid, std::vector<std::string> tokens)
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is handling enemy", 4);
+    if (tokens[0] == "210") {
+        createEnemy(uuid);
+        _EntitiesList[uuid]->setRelativePosX(std::stoi(tokens[5]), _WindowWidth);
+        _EntitiesList[uuid]->setRelativePosY(std::stoi(tokens[6]), _WindowHeight);
+    }
+}
+
+void Game::handleBullet(std::string uuid, std::vector<std::string> tokens)
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is handling bullet", 4);
+    if (tokens[0] == "220") {
+        createBullet(uuid);
+        std::cout << "created bullet" << std::endl;
+        _EntitiesList[uuid]->setRelativePosX(std::stoi(tokens[2]), _WindowWidth);
+        _EntitiesList[uuid]->setRelativePosY(std::stoi(tokens[3]), _WindowHeight);
+    }
+}
+
+void Game::createPlayer(std::string UUID)
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is creating player", 4);
+    if (this->_EntitiesList.find(UUID) != this->_EntitiesList.end())
+        return;
+    else {
+        _EntitiesList[UUID] = _EntitiesFactory->getEntity(EntityType::PLAYER);
+    }
+}
+
+void Game::createEnemy(std::string UUID)
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is creating enemy", 4);
+    if (this->_EntitiesList.find(UUID) != this->_EntitiesList.end())
+        return;
+    else {
+        _EntitiesList[UUID] = _EntitiesFactory->getEntity(EntityType::ENEMY);
+    }
+}
+
+void Game::createBullet(std::string UUID)
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is creating bullet", 4);
+    if (this->_EntitiesList.find(UUID) != this->_EntitiesList.end())
+        return;
+    else {
+        _EntitiesList[UUID] = _EntitiesFactory->getEntity(EntityType::BULLET);
+    }
+}
+
+void Game::destroyEntity(std::vector<std::string> tokens)
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is destroying entity", 4);
+    if (_EntitiesList.find(tokens[1]) != _EntitiesList.end()) {
+        if(tokens[1] == _PlayerId) {
+            _Running = false;
+            _GameOver = true;
+        }
+        _EntitiesList.erase(tokens[1]);
+    }
 }
 
 void Game::draw()
 {
-    if (!_Running)
+
+    
+    if (!_Running && !_GameOver)
         return;
 
-    // Assuming you have a method to draw text on the screen
-    // and a method to get the screen dimensions
+    if (_GameOver) {
+        DrawText("Game Over", _WindowWidth / 2 - 50, _WindowHeight / 2 - 50, 20, RED);
+        return;
+    }
 
-    std::string text = "server send " + _DataBuffer;
-    int fontsize = 20;
-    // Calculate the position to center the text
-    int textWidth = text.length() * fontsize;
-    int x = (_WindowWidth - textWidth) / 2;
-    int y = (_WindowHeight - fontsize) / 2;
-
-    DrawText(text.c_str(), x, y, fontsize, BLACK);
+    for (const auto &entity : _EntitiesList)
+        entity.second->draw();
 
     if (_DebugLogger)
         _DebugLogger->Log("Game is drawing", 4);
+}
+
+void Game::resetGame()
+{
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is resetting", 4);
+    _Running = false;
+    _GameOver = false;
+    _EntitiesList.clear();
+}
+
+void Game::clearEntities()
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is clearing entities", 4);
+    _EntitiesList.clear();
 }
 
 void Game::stop()
@@ -107,10 +300,21 @@ void Game::handleInput(int key, int pressedOrReleased)
         return;
     if (_DebugLogger)
         _DebugLogger->Log("Game is handling input", 4);
-    std::cout << "key : " << key << " pressedOrReleased : " << (pressedOrReleased == 0 ? "released" : "pressed") << std::endl;
+    (void)key;
+    (void)pressedOrReleased;
 }
 
 std::shared_ptr<EntitiesFactory> Game::getEntitiesFactory() const
 {
     return _EntitiesFactory;
+}
+
+bool Game::getShutDown()
+{
+    return _ShutDown;
+}
+
+bool Game::getGameOver()
+{
+    return _GameOver;
 }
--- client/src/game/game.hpp
+++ client/src/game/game.hpp
@@ -26,24 +26,47 @@ public:
     void initGame();
     void start();
     void update(std::string data);
+    void handleData(std::vector<std::string> tokens);
+
+    void handlePlayer(std::string UUID, std::vector<std::string> tokens);
+    void handleEnemy(std::string UUID, std::vector<std::string> tokens);
+    void handleBullet(std::string UUID, std::vector<std::string> tokens);
+
+    void createPlayer(std::string UUID);
+    void createEnemy(std::string UUID);
+    void createBullet(std::string UUID);
+
+    void destroyEntity(std::vector<std::string> tokens);
+
     void handleInput(int key, int pressedOrReleased);
 
     void draw();
-
+    void clearEntities();
     void stop();
+
+    void resetGame();
 
     bool isRunning() const;
 
     std::shared_ptr<EntitiesFactory> getEntitiesFactory() const;
 
+    bool getShutDown();
+    bool getGameOver();
+
 private:
     std::shared_ptr<EntitiesFactory> _EntitiesFactory; /**< Shared pointer to a EntitiesFactory object. */
+    std::map<std::string, std::shared_ptr<Entities>> _EntitiesList; /**< Map of menu types to AEntities objects. */
     std::shared_ptr<DebugLogger> _DebugLogger; /**< Shared pointer to a DebugLogger object. */
     std::shared_ptr<JsonParser> _JsonParser; /**< Shared pointer to a JsonParser object. */
     bool _Running; /**< Boolean to check if the game is running. */
 
+    bool _GameOver;
+    bool _ShutDown;
+
     int _WindowWidth; /**< The width of the window. */
     int _WindowHeight; /**< The height of the window. */
+
+    std::string _PlayerId;
 
     std::string _DataBuffer;
 };
