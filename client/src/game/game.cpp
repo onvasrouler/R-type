11d7009a553246e2be7d9680000f82229bffd216
aimericdrk <aimeric.rouyer@epitech.eu>
2024-11-01 11:09:50 +0100

    :sparkles: game is almost finished

--- client/src/game/game.cpp
+++ client/src/game/game.cpp
@@ -7,9 +7,13 @@
 
 #include "game.hpp"
 
-Game::Game(std::shared_ptr<JsonParser> jsonParser, std::shared_ptr<DebugLogger> debugLogger)
+Game::Game(std::shared_ptr<JsonParser> jsonParser, std::shared_ptr<DebugLogger> debugLogger, const int windowWidth, const int windowHeight)
 {
+    _WindowWidth = windowWidth;
+    _WindowHeight = windowHeight;
+    _DataBuffer = "";
     _DebugLogger = debugLogger;
+    _JsonParser = jsonParser;
     _EntitiesFactory = std::make_shared<EntitiesFactory>(jsonParser, debugLogger);
     _Running = false;
 }
@@ -19,27 +23,91 @@ void Game::setEntityFactoryJsonParser(std::shared_ptr<JsonParser> jsonParser)
     _EntitiesFactory->setJsonParser(jsonParser);
 }
 
+void Game::setDebugLogger(std::shared_ptr<DebugLogger> debugLogger)
+{
+    _DebugLogger = debugLogger;
+    _EntitiesFactory->setDebugLogger(debugLogger);
+}
+
+void Game::setWindowWidth(const int width)
+{
+    _WindowWidth = width;
+}
+
+void Game::setWindowHeight(const int height)
+{
+    _WindowHeight = height;
+}
+
+void Game::setWindowSize(const int width, const int height)
+{
+    this->setWindowWidth(width);
+    this->setWindowHeight(height);
+}
+
 void Game::initGame()
 {
     _EntitiesFactory->loadEntities(ASSETS_FILE_PATH);
-    _DebugLogger->Log("Game is initialized", 2);
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is initialized", 2);
 }
 
-void Game::run()
+void Game::start()
 {
     _Running = true;
-    _DebugLogger->Log("Game is running", 2);
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is running", 2);
+}
+
+void Game::update(std::string data)
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game received data", 4);
+    _DataBuffer = data;
+}
+
+void Game::draw()
+{
+    if (!_Running)
+        return;
+
+    // Assuming you have a method to draw text on the screen
+    // and a method to get the screen dimensions
+
+    std::string text = "server send " + _DataBuffer;
+    int fontsize = 20;
+    // Calculate the position to center the text
+    int textWidth = text.length() * fontsize;
+    int x = (_WindowWidth - textWidth) / 2;
+    int y = (_WindowHeight - fontsize) / 2;
+
+    DrawText(text.c_str(), x, y, fontsize, BLACK);
+
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is drawing", 4);
 }
 
 void Game::stop()
 {
     _Running = false;
-    _DebugLogger->Log("Game is stopped", 2);
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is stopped", 2);
 }
 
 bool Game::isRunning() const
 {
     return _Running;
+}
+
+void Game::handleInput(int key, int pressedOrReleased)
+{
+    if (!_Running)
+        return;
+    if (_DebugLogger)
+        _DebugLogger->Log("Game is handling input", 4);
+    std::cout << "key : " << key << " pressedOrReleased : " << (pressedOrReleased == 0 ? "released" : "pressed") << std::endl;
 }
 
 std::shared_ptr<EntitiesFactory> Game::getEntitiesFactory() const
--- client/src/game/game.hpp
+++ client/src/game/game.hpp
@@ -13,24 +13,37 @@
 
 class Game {
 public:
-    Game(std::shared_ptr<JsonParser> jsonParser = nullptr, std::shared_ptr<DebugLogger> debugLogger = nullptr);
+    Game(std::shared_ptr<JsonParser> jsonParser = nullptr, std::shared_ptr<DebugLogger> debugLogger = nullptr, const int windowWidth = 800, const int windowHeight = 600);
     ~Game() = default;
 
     void setEntityFactoryJsonParser(std::shared_ptr<JsonParser> jsonParser);
+    void setDebugLogger(std::shared_ptr<DebugLogger> debugLogger);
 
-    void setServerInfos(const std::string ip, const std::string port);
+    void setWindowWidth(const int width);
+    void setWindowHeight(const int height);
+    void setWindowSize(const int width, const int height);
 
     void initGame();
+    void start();
+    void update(std::string data);
+    void handleInput(int key, int pressedOrReleased);
 
-    void run();
+    void draw();
 
     void stop();
 
     bool isRunning() const;
+
     std::shared_ptr<EntitiesFactory> getEntitiesFactory() const;
 
 private:
     std::shared_ptr<EntitiesFactory> _EntitiesFactory; /**< Shared pointer to a EntitiesFactory object. */
     std::shared_ptr<DebugLogger> _DebugLogger; /**< Shared pointer to a DebugLogger object. */
+    std::shared_ptr<JsonParser> _JsonParser; /**< Shared pointer to a JsonParser object. */
     bool _Running; /**< Boolean to check if the game is running. */
+
+    int _WindowWidth; /**< The width of the window. */
+    int _WindowHeight; /**< The height of the window. */
+
+    std::string _DataBuffer;
 };
